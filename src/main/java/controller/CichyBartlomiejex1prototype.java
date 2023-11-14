0e8f7c4a7005eeac8528fa80a9233edadf8571f3
barcic2948 <barcic2948@gmail.com>
2023-11-14 18:14:06 +0100

    View fix

--- src/main/java/controller/CichyBartlomiejex1prototype.java
+++ src/main/java/controller/CichyBartlomiejex1prototype.java
@@ -16,7 +16,6 @@ public class CichyBartlomiejex1prototype {
      */
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         MatrixController matrixController = new MatrixController();
 
         matrixController.run(args);
